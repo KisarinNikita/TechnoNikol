cdf93fa8529524efc9c4dd82a1ee348fede3fa39
KisarinNikita <kisarinnnikita@gmail.com>
2016-11-16 05:49:01 +0700

    process

--- js/main.js
+++ js/main.js
@@ -1,5 +1,3 @@
-
-
 var slider = function(){
   $('.slider-for').slick({
     slidesToShow: 1,
@@ -17,6 +15,40 @@ var slider = function(){
   });
 };
 
+var counter = function() {
+  $('#plus').click( function () {
+    var i = +$('#count').text() + 1;
+    $('#count').text(i);
+  });
+  $('#minus').click( function () {
+    var i = +$('#count').text() -1;
+    if ( i < 0){
+      i = +$('#count').text() + 0;
+    };
+    $('#count').text(i);
+  });
+};
+
+var nicecheckboxes = function(){
+  $('.item_checkbox').each(function(){
+      var checkbox = $(this).find('input[type=checkbox]');
+      if(checkbox.prop("checked")) $(this).addClass("check_active");
+  });
+
+  $('.item_checkbox').click(function(){
+      var checkbox = $(this).find('input[type=checkbox]');
+      if(checkbox.prop("checked")){
+          $(this).removeClass("check_active");
+          checkbox.prop("checked", false);
+      }else{
+          $(this).addClass("check_active");
+          checkbox.prop("checked", true);
+      }
+  });
+};
+
 $(document).ready(function(){
   slider();
+  counter();
+  nicecheckboxes();
 });
