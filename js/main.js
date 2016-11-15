502e430e5093770c5790ed609442ad297b19edb8
KisarinNikita <kisarinnnikita@gmail.com>
2016-11-15 20:39:43 +0700

    process
    firs commit

---
+++ js/main.js
@@ -0,0 +1,23 @@
+
+
+var slider = function(){
+  $('.slider-for').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: false,
+    fade: true,
+    asNavFor: '.slider-nav'
+  });
+  $('.slider-nav').slick({
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    asNavFor: '.slider-for',
+    dots: true,
+    centerMode: true,
+    focusOnSelect: true
+  });
+};
+
+$(document).ready(function(){
+  slider();
+});
